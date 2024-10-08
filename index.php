366707b882af90f057cb0c940b5bd99541e13377
K0087 <91182660+K0087@users.noreply.github.com>
2024-10-08 20:38:43 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,26 +1,23 @@
-<?php require_once('./config.php'); ?>
+<?php require_once('../config.php'); ?>
  <!DOCTYPE html>
 <html lang="en" class="" style="height: auto;">
 <?php require_once('inc/header.php') ?>
-  <body class="layout-top-nav dark-mode" data-new-gr-c-s-check-loaded="14.991.0" data-gr-ext-installed="" style="height: auto;">
+  <body class="sidebar-mini layout-fixed control-sidebar-slide-open layout-navbar-fixed dark-mode sidebar-mini-md sidebar-mini-xs" data-new-gr-c-s-check-loaded="14.991.0" data-gr-ext-installed="" style="height: auto;">
     <div class="wrapper">
      <?php require_once('inc/topBarNav.php') ?>
+     <?php require_once('inc/navigation.php') ?>
      <?php if($_settings->chk_flashdata('success')): ?>
       <script>
         alert_toast("<?php echo $_settings->flashdata('success') ?>",'success')
       </script>
-      <?php endif;?>   
-      <?php if($_settings->chk_flashdata('warning')): ?>
-      <script>
-        alert_toast("<?php echo $_settings->flashdata('warning') ?>",'warning')
-      </script>
-      <?php endif;?>  
+      <?php endif;?>    
      <?php $page = isset($_GET['page']) ? $_GET['page'] : 'home';  ?>
       <!-- Content Wrapper. Contains page content -->
-      <div class="content-wrapper pt-3  mx-0 px-0" style="min-height: 567.854px;">
+      <div class="content-wrapper bg-dark pt-3" style="min-height: 567.854px;">
      
         <!-- Main content -->
-        <section class="content mx-0 px-0">
+        <section class="content  text-dark">
+          <div class="container-fluid">
             <?php 
               if(!file_exists($page.".php") && !is_dir($page)){
                   include '404.html';
@@ -32,6 +29,7 @@
 
               }
             ?>
+          </div>
         </section>
         <!-- /.content -->
   <div class="modal fade" id="confirm_modal" role='dialog'>
