09a0870d1811136f8d9cb556a1f738593af59597
Dinesh Kumar <mapmydestination@gmail.com>
2024-11-02 18:40:57 +0530

    merged

--- inc/sess_auth.php
+++ inc/sess_auth.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php 
 // if (session_status() == PHP_SESSION_NONE) {
 //     session_start();
@@ -16,27 +15,3 @@ if(!isset($_SESSION['userdata']) && !strpos($link, 'login.php')){
 if(isset($_SESSION['userdata']) && strpos($link, 'login.php')){
 	redirect('index.php');
 }
-=======
-<?php 
-if (session_status() == PHP_SESSION_NONE) {
-    session_start();
-}
-if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') 
-    $link = "https"; 
-else
-    $link = "http"; 
-$link .= "://"; 
-$link .= $_SERVER['HTTP_HOST']; 
-$link .= $_SERVER['REQUEST_URI'];
-if(!isset($_SESSION['userdata']) && !strpos($link, 'login.php')){
-	redirect('admin/login.php');
-}
-if(isset($_SESSION['userdata']) && strpos($link, 'login.php')){
-	redirect('admin/index.php');
-}
-$module = array('','admin','faculty','student');
-if(isset($_SESSION['userdata']) && (strpos($link, 'index.php') || strpos($link, 'admin/')) && $_SESSION['userdata']['login_type'] !=  1){
-	echo "<script>alert('Access Denied!');location.replace('".base_url.$module[$_SESSION['userdata']['login_type']]."');</script>";
-    exit;
-}
->>>>>>> repo2/main
